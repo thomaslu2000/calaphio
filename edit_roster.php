26f32c3e891ee4fff139094f9903d2344c5b8c61
Benjamin Le <kantas92@berkeley.edu>
2013-09-13 18:01:04 -0700

    Resize Img

--- edit_roster.php
+++ edit_roster.php
@@ -2,6 +2,7 @@
 require("include/includes.php");
 require("include/Calendar.class.php");
 require("include/Template.class.php");
+require("include/Simple.class.php");
 Template::print_head(array());
 Template::print_body_header('Brothers', 'ACCOUNT');
 if (!$g_user->process_change_passphrase()) {
@@ -15,28 +16,38 @@ echo "<br />";
 
 if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
   if ($file = getimagesize($_FILES["upfile"]["tmp_name"])) {
-	if($_FILES["upfile"]["size"] <= 100000 && $file[2] == IMAGETYPE_PNG) {
+	if($file[2] == IMAGETYPE_PNG) {
 		if (file_exists("./face/" . $g_user->data['user_id'] . ".jpg")) {
 			unlink("./face/" . $g_user->data[user_id] . ".jpg");
 		} 
 		if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "face/" . $g_user->data['user_id'] . ".png")) {
-    			chmod("files/" . $g_user->data['user_id'] . ".png", 0644);
+				$filename = "files/" . $g_user->data['user_id'] . ".png";
+    			$image = new SimpleImage();
+    			$image->load($filename); 
+    			$image->resizeToWidth(400); 
+    			$image->save($filename);
+    			chmod($filename, 0644);
     			echo $_FILES["upfile"]["name"] . " upload successful";
 		} else {
 			echo "Can not upload the photo for an unknown reason";
 		}
-	} else if ($_FILES["upfile"]["size"] <= 100000 && $file[2] == IMAGETYPE_JPEG) { 
+	} else if ($file[2] == IMAGETYPE_JPEG) { 
 		if (file_exists("./face/" . $g_user->data['user_id'] . ".png")) {
 			unlink("./face/" . $g_user->data['user_id'] . ".png");
 		}
 		if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "face/" . $g_user->data['user_id'] . ".jpg")) {
-    			chmod("files/" . $g_user->data['user_id'] . ".jpg", 0644);
+    			$filename = "files/" . $g_user->data['user_id'] . ".jpg";
+    			$image = new SimpleImage();
+    			$image->load($filename); 
+    			$image->resizeToWidth(400); 
+    			$image->save($filename);
+    			chmod($filename, 0644);
     			echo $_FILES["upfile"]["name"] . " upload successful";
 		} else {
 			echo "Can't upload the photo for an unknown reason";
 		}
   	} else {
-    	     echo "Can't upload the photo (check the size/format)";
+    	     echo "Can't upload the photo (check that the format is PNG or JPG)";
  	 } 
   }else {
   	echo "Not a photo";
