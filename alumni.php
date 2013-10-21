5124023a8bd2f889182008850e478c4aef4ccc32
Benjamin Le <kantas92@berkeley.edu>
2013-10-20 23:35:13 -0700

    Alumni Mentors can be seen by only apo users

--- alumni.php
+++ alumni.php
@@ -6,8 +6,10 @@ Template::print_head(array());
 Template::print_body_header('Brothers', 'ALUMNI');
 ?>
 <p>Visit the <a href="http://www.facebook.com/group.php?gid=101430558612">Gamma Gamma Alumni Association Facebook Group</a>.</p>
+<?php if ($g_user->is_logged_in()): ?>
 <h2> Alumni Mentors! </h2>
 <iframe width="940" height="500" src="https://docs.google.com/spreadsheet/pub?key=0Ar71tLU30hK3dHJCNkJfWE5xeXN1MkhMZFpSNUVlNHc&output=html"></iframe>
+<?php endif ?>
 <?php
 Template::print_body_footer();
 Template::print_disclaimer();
