d88da8a4559cf500ae094bb6f960cae01b539804
Benjamin Le <BenjaminHoanLe@gmail.com>
2014-01-06 21:24:52 -0800

    Remove break, Fix Type

--- include/Calendar.class.php
+++ include/Calendar.class.php
@@ -587,7 +587,7 @@ DOCHERE_print_add_people;
 		
 		if (isset($_REQUEST['function'])) {
 				$add_user_id = $_POST['add-person'];
-				$remove_user_id = $_POST['remove_person'];
+				$remove_user_id = $_POST['remove-person'];
 				$description = "Replaced User " . $remove_user_id . " with User " . $add_user_id;
 				$timestamp = date("Y-m-d H:i:s");
 				$query = new Query("start transaction");
@@ -595,7 +595,6 @@ DOCHERE_print_add_people;
 				$query = new Query(sprintf("DELETE FROM %scalendar_attend WHERE event_id=%d AND user_id=%d LIMIT 1", TABLE_PREFIX, $event_id, $remove_user_id));
 				$query = new Query(sprintf("INSERT INTO %sevent_audit_trail SET event_id=%d, user_id=%d, timestamp='%s', description='%s'", TABLE_PREFIX, $event_id, $g_user->data['user_id'], $timestamp, $description));
 				$query = new Query("commit");
-				break;
 		}
 		
 		$evaluate = isset($_REQUEST['evaluate']) && $_REQUEST['evaluate'] ? "<input type=\"hidden\" name=\"evaluate\" value=\"true\" />" : "";
