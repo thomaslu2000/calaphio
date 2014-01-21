51e10656b8bbc180152786923bd8efda741b2f5c
Jimmy(Ngoc) B. Tran <ngoctranfire@gmail.com>
2014-01-21 04:45:46 -0800

    View Maniacs

--- gg_maniacs.php
+++ gg_maniacs.php
@@ -8,7 +8,28 @@ require("include/GGManiacNag.class.php");
 Template::print_head(array("site.css", "calendar.css", "excel.css"));
 Template::print_body_header('Home', 'NEWS');
 ?>
-
+<h1 class="center"></h1>
+<div class="newsItem">
+    <h2>DE Semester GG Maniacs!</h2>
+    <div class="collage-container">
+	<div class="collage-pictures">
+		<div class="person-picture">
+			<img src="documents/sp14/rush/lakana_bun.jpg"></img>
+			<p class="center">Lakana Bun</p>
+			
+		</div>
+		<div class="person-picture">
+			<img src="documents/sp14/rush/sharon_wang.jpg"></img>
+			<p class="center">Sharon Wang</p>
+		</div>
+		<div class="person-picture">
+			<img src="documents/sp14/rush/wiemond_wu.jpg"></img>
+			<p class="center">Wiemond Wu</p>
+		</div>
+	</div>
+	<div style="clear: left;"></div>
+    </div>
+</div>
 <?php
 Template::print_body_footer();
 Template::print_disclaimer();
