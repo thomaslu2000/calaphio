ea0cda9cacb7533f893664a4961321de5a1873e2
Jimmy(Ngoc) B. Tran <ngoctranfire@gmail.com>
2014-01-21 05:00:13 -0800

    Changed Maniacs

--- gg_maniacs.php
+++ gg_maniacs.php
@@ -8,24 +8,52 @@ require("include/GGManiacNag.class.php");
 Template::print_head(array("site.css", "calendar.css", "excel.css"));
 Template::print_body_header('Home', 'NEWS');
 ?>
-<h1 class="center"></h1>
+<h1 class="center">Gamma Gamma Maniacs!</h1>
 <div class="newsItem">
-    <h2>DE Semester GG Maniacs!</h2>
+    <h2 class="center">DE Semester GG Maniacs!</h2>
     <div class="collage-container">
 	<div class="collage-pictures">
 		<div class="person-picture">
-			<img src="documents/sp14/rush/lakana_bun.jpg"></img>
-			<p class="center">Lakana Bun</p>
+			<p class="center">Sridatta Thatipamala </p>
+		</div>
+		<div class="person-picture">
+			<p class="center">Kevin Nguyen</p>
+		</div>
+		<div class="person-picture">
+			<p class="center">Zachary Lee</p>
+		</div>
+		<div class="person-picture">
+			<p class="center">Ben Le</p>
 			
 		</div>
 		<div class="person-picture">
-			<img src="documents/sp14/rush/sharon_wang.jpg"></img>
-			<p class="center">Sharon Wang</p>
+			<p class="center">Christopher Ching</p>
 		</div>
 		<div class="person-picture">
-			<img src="documents/sp14/rush/wiemond_wu.jpg"></img>
-			<p class="center">Wiemond Wu</p>
+			<p class="center">Jejee Alisa Hasdarngkul</p>
 		</div>
+		<div class="person-picture">
+			<p class="center">TinOi Lui</p>
+		</div>
+		
+	</div>
+	<div style="clear: left;"></div>
+    </div>
+</div>
+<div class="newsItem">
+    <h2 class="center">DE Semester Pledge Maniacs!</h2>
+    <div class="collage-container">
+	<div class="collage-pictures">
+		<div class="person-picture">
+			<p class="center">Sharon Wang </p>
+		</div>
+		<div class="person-picture">
+			<p class="center">Lakana Bun</p>
+		</div>
+		<div class="person-picture">
+			<p class="center">Dennis Lee</p>
+		</div>
+
 	</div>
 	<div style="clear: left;"></div>
     </div>
