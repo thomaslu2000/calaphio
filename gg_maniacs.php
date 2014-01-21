ecbd8e88c7cdbdb312aa42b15e1cb91edd597c30
Jimmy(Ngoc) B. Tran <ngoctranfire@gmail.com>
2014-01-21 04:29:29 -0800

    Changed Directory to Lead to New Link

---
+++ gg_maniacs.php
@@ -0,0 +1,10 @@
+<?php
+require("include/includes.php");
+require("include/Calendar.class.php");
+require("include/Template.class.php");
+require("include/Shoutbox.class.php");
+require("include/EvalNag.class.php");
+require("include/GGManiacNag.class.php");
+Template::print_head(array("site.css", "calendar.css", "excel.css"));
+Template::print_body_header('Home', 'NEWS');
+?>
