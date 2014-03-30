063f8de07d92098752cb4924a7f7ff0682701835
ngoctranfire <ngoctranfire@gmail.com>
2014-03-30 03:07:18 -0700

    Fixed Excomm Officers Page to Link Correctly

--- excomm.php
+++ excomm.php
@@ -15,52 +15,52 @@ echo <<<DOCHERE_print_gg_maniac_poll_create
 			<a class="pic" href="excomm/sp14/president.jpg">
 			<img src="excomm/sp14/president.jpg" id="excomm" alt="President, Jeff Ma">
 			</a>
-			<p align='center'> <b>PRESIDENT</b> <br><a href="profile.php?user_id=1368">Jeff Ma</a><br>Pledged Jennifer Sun <br> <a href="mailto:president@calaphio.com">president@calaphio.com</a></p>
+			<p align='center'> <b>PRESIDENT</b> <br><a href="profile.php?user_id=1412">Jeff Ma</a><br>Pledged Jennifer Sun <br> <a href="mailto:president@calaphio.com">president@calaphio.com</a></p>
 
 			</li>
 			<li>
 			<a class="pic" href="excomm/sp14/admin.jpg">
 				<img src="excomm/sp14/admin.jpg" id="excomm" alt="Administrative Vice President, Ngoc Tran">
 			</a>
-			<p align='center'> <b>ADMINISTRATIVE VICE PRESIDENT</b> <br><a href="profile.php?user_id=1411">Ngoc Tran</a><br>Pledged Maura Harty<br> <a href="mailto:admin-vp@calaphio.com">admin-vp@calaphio.com</a></p>
+			<p align='center'> <b>ADMINISTRATIVE VICE PRESIDENT</b> <br><a href="profile.php?user_id=1584">Ngoc Tran</a><br>Pledged Maura Harty<br> <a href="mailto:admin-vp@calaphio.com">admin-vp@calaphio.com</a></p>
 			</li>
 			
 			<li>
 			<a class="pic" href="excomm/sp14/membership.jpg">
 				<img src="excomm/sp14/membership.jpg" id="excomm"" alt="Membership Vice President, Stephanie Chan ">
 			</a>
-			<p align='center'> <b>MEMBERSHIP VICE PRESIDENT</b> <br><a href="profile.php?user_id=1276">Stephanie Chan</a><br>Pledged Jennifer Sun<br> <a href="mailto:membership-vp@calaphio.com">membership-vp@calaphio.com</a> </p>
+			<p align='center'> <b>MEMBERSHIP VICE PRESIDENT</b> <br><a href="profile.php?user_id=1406">Stephanie Chan</a><br>Pledged Jennifer Sun<br> <a href="mailto:membership-vp@calaphio.com">membership-vp@calaphio.com</a> </p>
 			</li>
 
 			<li>
 			<a class="pic" href="excomm/sp14/service.jpg">
 				<img src="excomm/sp14/service.jpg" id="excomm" alt="Service Vice President, Susan Guan">
 			</a>
-			<p align='center'> <b>SERVICE VICE PRESIDENT</b> <br><a href="profile.php?user_id=1305">Susan Guan</a><br>Pledged Maura Harty<br> <a href="mailto:service-vp@calaphio.com">service-vp@calaphio.com</a> </p>
+			<p align='center'> <b>SERVICE VICE PRESIDENT</b> <br><a href="profile.php?user_id=1426">Susan Guan</a><br>Pledged Maura Harty<br> <a href="mailto:service-vp@calaphio.com">service-vp@calaphio.com</a> </p>
 			</li>
 			<li>
 			<a class="pic" href="excomm/sp14/finance.jpg">
 				<img src="excomm/sp14/finance.jpg" id="excomm" alt="Finance Vice President, Jane Tam">
 			</a>
-			<p align='center'> <b>FINANCE VICE PRESIDENT</b><br><a href="profile.php?user_id=1405">Jane Tam</a><br>Pledged Maura Harty<br> <a href="mailto:finance-vp@calaphio.com">finance-vp@calaphio.com</a></p>
+			<p align='center'> <b>FINANCE VICE PRESIDENT</b><br><a href="profile.php?user_id=1437">Jane Tam</a><br>Pledged Maura Harty<br> <a href="mailto:finance-vp@calaphio.com">finance-vp@calaphio.com</a></p>
 			</li>
 			<li>
 			<a class="pic" href="excomm/sp14/fellowship.jpg">
 				<img src="excomm/sp14/fellowship.jpg" id="excomm" alt="Fellowship Vice President, Sarah Wang">
 			</a>
-			<p align='center'> <b>FELLOWSHIP VICE PRESIDENT</b><br><a href="profile.php?user_id=1387">Sarah Wang</a><br>Pledged Kingsley Kuang<br> <a href="mailto:fellowship-vp@calaphio.com">fellowship-vp@calaphio.com</a></p>
+			<p align='center'> <b>FELLOWSHIP VICE PRESIDENT</b><br><a href="profile.php?user_id=2074">Sarah Wang</a><br>Pledged Kingsley Kuang<br> <a href="mailto:fellowship-vp@calaphio.com">fellowship-vp@calaphio.com</a></p>
 			</li>
 			<li>
 			<a class="pic" href="excomm/sp14/pledgemaster.jpg">
 				<img src="excomm/sp14/pledgemaster.jpg" id="excomm" alt="Pledgemaster, Vivian Nguyen">
 			</a>
-			<p align='center'> <b>PLEDGEMASTER</b> <br><a href="profile.php?user_id=1408">Vivian Nguyen</a><br>Pledged Jennifer Sun<br> <a href="mailto:pledgemaster@calaphio.com">pledgemaster@calaphio.com</a></p>
+			<p align='center'> <b>PLEDGEMASTER</b> <br><a href="profile.php?user_id=1386">Vivian Nguyen</a><br>Pledged Jennifer Sun<br> <a href="mailto:pledgemaster@calaphio.com">pledgemaster@calaphio.com</a></p>
 			</li>
 			<li>
 			<a class="pic" href="excomm/sp14/historian.jpg">
 				<img src="excomm/sp14/historian.jpg" id="excomm" alt="Chapter Historian, Kelsey Chan">
 			</a>
-			<p align='center'> <b>CHAPTER HISTORIAN</b><br><a href="profile.php?user_id=1416">Kelsey Chan</a><br>Pledged Kingsley Kuang <br> <a href="mailto:historian@calaphio.com">historian@calaphio.com</a></p>
+			<p align='center'> <b>CHAPTER HISTORIAN</b><br><a href="profile.php?user_id=2055">Kelsey Chan</a><br>Pledged Kingsley Kuang <br> <a href="mailto:historian@calaphio.com">historian@calaphio.com</a></p>
 			</li>
 			</ul>
 	</div>
