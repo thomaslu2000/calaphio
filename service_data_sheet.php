5935dc5b1386e8370d7c59b1559bc22db83f1dcc
ngoctranfire <ngoctranfire@gmail.com>
2014-04-13 04:45:24 -0700

    debugging

--- service_data_sheet.php
+++ service_data_sheet.php
@@ -145,7 +145,7 @@ $row = $query->fetch_row();
 $spring14_fellowships = $row['count'];
 
 echo 'Check here!';
-$query_attending = $this->query_attending_services('2012-8-28', '2014-4-29');
+$query_attending = $this->query_attending_services('2012828', '2014429');
 echo 'Check here 2';
 $row_attending = $query_attending->fetch_row();
 echo 'Check here 3';
