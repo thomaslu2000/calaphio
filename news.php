2a7a66acec753d2a11aeb11ea84b20d7ad7fb93d
ngoctranfire <ngoctranfire@gmail.com>
2014-04-15 16:39:12 -0700

    Direct links to profile now

--- news.php
+++ news.php
@@ -52,8 +52,8 @@ if (!$g_user->is_logged_in()) {
         get submitted will be: <br/><br/>
         We have submissions from the following people:
         <ul style="margin-left: 1em; list-style: inside disc; margin-bottom: 1.5em;">
-            <li><a href="roster.php?function=Search&user_id=1673">Pooja Shah (KK)</a>for Fellowship VP! Submitted 12:34pm, Monday, April 14, 2014</li>
-            <li><a href="roster.php?function=Search&user_id=1400">Debra Yan (JS)</a> for Pledgemaster! Submitted 3:03pm, Tuesday, April 15, 2014.</li>
+            <li><a href="/profile.php?user_id=1673">Pooja Shah (KK)</a>for Fellowship VP! Submitted 12:34pm, Monday, April 14, 2014</li>
+            <li><a href="profile.php?user_id=1400">Debra Yan (JS)</a> for Pledgemaster! Submitted 3:03pm, Tuesday, April 15, 2014.</li>
         </ul>
         Please go to the link below to see it! <br/><br/>
         <a target="_blank" style="margin-left: 1em;"href="sp14platforms.php">Fall 2014 Platforms</a>
@@ -68,7 +68,7 @@ if (!$g_user->is_logged_in()) {
     <p style="margin-bottom: 1.5em">Ever since pledging APO during Dave Emery(DE) semester, she has been a great help and contributed a lot of Gamma Gamma's active chapter.
     This semester, she is a chair for College Day! She is rather shy, but if you see her around, don't be afraid to engage
     in a conversation with her because she is extremely friendly. </p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="/profile.php?user_id=1584">Ngoc Tran (MH)</a></p>
 </div>
 <?php if($g_user->is_logged_in()): ?>
 <!--<div class="newsItem">
@@ -93,7 +93,7 @@ if (!$g_user->is_logged_in()) {
     <p style="margin-bottom: em"> Here is our wonderful <a href="https://drive.google.com/file/d/0B2qOHCQXVkc3QThHY3VLdXRtcTA/edit?usp=sharing">CM 6 STYLUS! </a> Read it and enjoy a great laugh! </p>
 
     <iframe width="420" height="315" src="//www.youtube.com/embed/gYSu6pkV9F0" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 
@@ -106,7 +106,7 @@ if (!$g_user->is_logged_in()) {
 	semester, he stepped up to become the Spirit Chair and is leading the Roll Call Dance Team! I can tell we are going to have an amazing dance team already! </p>
 	<p style="margin-bottom: 1.5em">If you see Ryan, congratulate him and talk to him if you don't know him! I promise you won't regret it! Everyone who knows Ryan 
 		feels very lucky because he is one of the coolest and down to earth people you will ever meet!</p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if($g_user->is_logged_in()): ?>
 <div class="newsItem">
@@ -119,7 +119,7 @@ if (!$g_user->is_logged_in()) {
     <p style="margin-bottom: em"> Here is our wonderful <a href="https://drive.google.com/file/d/0B2qOHCQXVkc3amZNZWV2dXdieWc/edit?usp=sharing">CM 5 STYLUS! </a> Read it and enjoy a great laugh! </p>
 
     <iframe width="420" height="315" src="//www.youtube.com/embed/4Cem30N-gRc" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 <div class="newsItem">
@@ -129,7 +129,7 @@ if (!$g_user->is_logged_in()) {
 	<p style="margin-bottom: 1.5em">Please congratulate Annie Ferguson for winning GG Maniac! Annie pledged Jennifer Sun semester and was Historian last semester. She is currently an aunt for Whale I Am 
 	and chairing YTA Mosswood where she helps tutor little kids! She has always been a great friend to many in APhiO and is always involved to help her family out! </p>
 	<p style="margin-bottom: 1.5em">When you see Annie, thank her to show your appreciation! Maybe give her a ducky or two as well!</p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if($g_user->is_logged_in()): ?>
 <div class="newsItem">
@@ -142,7 +142,7 @@ if (!$g_user->is_logged_in()) {
 	<p style="margin-bottom: 1.5em"><a href="https://docs.google.com/forms/d/1D1W2-wtDM1mIIiEG8eQKJqi6K_UiGAMU3adFIiYtvwg/viewform">Submit to Caption Contest!</a></p>
 	<p style="margin-bottom: 1.5em"><b>Also, if you got something dirt or something funny you want everyone else to know, submit to the <a href="https://docs.google.com/forms/d/1ELsVwAiney9Rm7AoXn7CZYv7t68iXD749siiO3Cx-JQ/viewform">Stylus Dirt Box!</a>
 		It really helps to make a better Caption Contest!</b></p>
-	<p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+	<p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <div class="newsItem">
     <h2>Notes From CM 4</h2>
@@ -154,7 +154,7 @@ if (!$g_user->is_logged_in()) {
     <p style="margin-bottom: em"> Here is our wonderful <a href="https://drive.google.com/file/d/0B2qOHCQXVkc3ZVpEZ2tOYmR0UEU/edit?usp=sharing">CM 4 STYLUS! </a> Read it and enjoy a great laugh! </p>
 
     <iframe style="margin-bottom: 1em" width="480" height="360" src="//www.youtube.com/embed/ixXIrP9Vgb8" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 <div class="newsItem">
@@ -164,7 +164,7 @@ if (!$g_user->is_logged_in()) {
 	<p style="margin-bottom: 1.5em">Please congratulate James Wang for winning GG Maniac! James is like a teddy bear and is loved by his family and friends in Alpha Phi Omega! James pledged Maura Harty semester
 and is now bigging again this semester! Whenever you need someone to fill up a spot for you at a fellowship, fundraiser, or service, James will be there to help you out! </p>
 	<p style="margin-bottom: 1.5em">When you see James, give him a hug and show him your appreciation. For those who do end up being his littles, you will really love having him around! </p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if ($g_user->is_logged_in()): ?>
 <div class="newsItem">
@@ -177,7 +177,7 @@ and is now bigging again this semester! Whenever you need someone to fill up a s
     <p style="margin-bottom: em"> Here is our wonderful <a href="https://docs.google.com/file/d/0B2qOHCQXVkc3WkVIbjVwSldRc28/edit">CM 3 STYLUS!</a> Read it and enjoy a great laugh! </p>
 
     <iframe style="margin-bottom: 1em" width="480" height="360" src="//www.youtube.com/embed/58kugX6pBiA" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 <div class="newsItem">
@@ -188,7 +188,7 @@ and is now bigging again this semester! Whenever you need someone to fill up a s
 does as much, if not more than a big should. She pledged JS semester and was Gamma Gamma's Finance Vice President last semester! </p>
 	<p style="margin-bottom: 1.5em">Without her, the chapter wouldn't have made it this far! If you see her, please
 remember to thank her and congratulate her because she deserves it after putting in so much work!</p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if ($g_user->is_logged_in()): ?>
 
@@ -202,7 +202,7 @@ remember to thank her and congratulate her because she deserves it after putting
     
     </p>
     <iframe style="margin-bottom: 1em" width="480" height="360" src="//www.youtube.com/embed/uygn3MZplUw" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <div class="newsItem">
     <h2>Notes From CM 1</h2>
@@ -216,7 +216,7 @@ remember to thank her and congratulate her because she deserves it after putting
      2) <a href="https://drive.google.com/file/d/0B2qOHCQXVkc3X1pxLWZRcmpmTUU/edit?usp=sharing">Pledge Requirements</a>
     </p>
     <iframe style="margin-bottom: 1em" width="480" height="360" src="//www.youtube.com/embed/GZ2u6Cd-lvg" frameborder="0" allowfullscreen></iframe>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 <div class="newsItem">
@@ -227,7 +227,7 @@ remember to thank her and congratulate her because she deserves it after putting
 	Ever since then, she has served as Service Trainer, Service Vice President, and eventually becoming President in the Spring of 2011 and also chaired Regionals! Although she has graduated,
 	she still contributes to the chapter and was recently a parent for Sour Ratch Kids in the Spring of 2013. The chapter is proud to have Courtney as our namesake!</p>
 	<p style="margin-bottom: 1.5em">If you ever see Courtney, please join me in congratulating her and strike a conversation to get a feel of her dazzling personality!</p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if ($g_user->is_logged_in() && !$g_user->is_pledge()): ?>
 
@@ -236,14 +236,14 @@ remember to thank her and congratulate her because she deserves it after putting
     <p class="date">January 21, 2014</p>
     <p style="margin-bottom: 1.5em">Check out our budget below!</p>
 	<p style="margin-bottom: 1.5em"><a href="https://docs.google.com/spreadsheet/ccc?key=0AjUhxDpscmUidDQtVkNWVVFpSWhvMzY3Z3ZOcEVWX1E&usp=sharing#gid=0">Spring 2014 Budget!</a></p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <div class="newsItem">
 	<h2>Flyer for Rush!</h2>
     <p class="date">January 21, 2014</p>
     <p style="margin-bottom: 1.5em">Sign up for flyering and get some good <strong>CM</strong> pledges!</p>
 	<p style="margin-bottom: 1.5em"><a href="https://docs.google.com/spreadsheets/d/1SSCfj2-rx_HMjs2zx9N-nGbKQk1rf7iPkTyZw-s7SXM/edit#gid=1267904948">Flyering and Chalking Sign Ups!</a></p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 
 </div>
 
@@ -271,7 +271,7 @@ remember to thank her and congratulate her because she deserves it after putting
 	<div style="clear: left;"></div>
     </div>
     <p>I know they will recruit amazing new additions to the chapter!</p>
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php if ($g_user->is_logged_in()): ?>
 <div class="newsItem">
@@ -321,7 +321,7 @@ remember to thank her and congratulate her because she deserves it after putting
 	<div style="clear: left;"></div>
     </div>
 
-    <p>-<a href="roster.php?function=Search&user_id=1584">Ngoc Tran (MH)</a></p>
+    <p>-<a href="profile.php?user_id=1400">Ngoc Tran (MH)</a></p>
 </div>
 <?php endif ?>
 <a href="news_fa13.php">Older News ></a>
