fd94c80c4d6a12ccead4a48fc61f9c93d3dc9fdc
ngoctranfire <ngoctranfire@gmail.com>
2014-04-15 16:23:46 -0700

    Added Platforms

--- news.php
+++ news.php
@@ -48,9 +48,11 @@ if (!$g_user->is_logged_in()) {
         <li><a target="_blank" href="fa09platforms.php">Fall 2009 Platforms</a></li>
         <li><a target="_blank" href="sp09platforms.php">Spring 2009 Platforms</a></li>
     </ul></p>
-    <p>Submit all Fall 2014 Platforms to me at admin-vp@calaphio.com. In addition, I will be doing LIVE UPDATES as soon as a candidate submits their platforms for Fall 2014 Excomm. The link to view Spring 2014 Platforms as soon as they
+    <p>Submit all Spring 2014 Platforms to me at admin-vp@calaphio.com. In addition, I will be doing LIVE UPDATES as soon as a candidate submits their platforms for Fall 2014 Excomm. The link to view Spring 2014 Platforms as soon as they
         get submitted will be: <br/>
-        <a target="_blank" style="margin-left: 1em;"href="fa14platforms.php">Fall 2014 Platforms</a>
+        We have one submission from <a href="roster.php?function=Search&user_id=1673">Pooja Shah (KK)</a>. Please go to the link below to see it! <br/>
+        We have another submission from <a href="roster.php?function=Search&user_id=1400">Debra Yan (JS)</a>. Please go to the link below to see it! <br/>
+        <a target="_blank" style="margin-left: 1em;"href="sp14platforms.php">Fall 2014 Platforms</a>
         <br/>
     </p>
 </div>
