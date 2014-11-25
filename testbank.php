fd8fe3d803dcba4ef203998b32ab695b0fe11d3d
Bofan Chen <bofanchen1@gmail.com>
2014-11-24 16:49:03 -0800

    fixing

--- testbank.php
+++ testbank.php
@@ -7,8 +7,8 @@ Template::print_body_header('Brothers', 'TESTBANK');
 if (!$g_user->is_logged_in()) {
 	trigger_error("You must be logged in to view the testbank.", E_USER_ERROR);
 } else {
-	echo "<ul style="list-style: disc inside; margin-left: 6px;">"
-	echo "<li><a href="testbank_cs.php">Computer Science</a></li>"
+	echo "<ul style=\"list-style: disc inside; margin-left: 6px;\">\n"
+	echo "<li><a href=\"testbank_cs.php\">Computer Science</a></li>"
 	echo "</ul>"
 }
 Template::print_body_footer();
