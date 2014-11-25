3c5baff8347a7662c26da8010943506cf550e038
Bofan Chen <bofanchen1@gmail.com>
2014-11-24 16:46:21 -0800

    fixing

--- testbank.php
+++ testbank.php
@@ -7,14 +7,9 @@ Template::print_body_header('Brothers', 'TESTBANK');
 if (!$g_user->is_logged_in()) {
 	trigger_error("You must be logged in to view the testbank.", E_USER_ERROR);
 } else {
-	echo "<ul style=\"list-style: disc inside; margin-left: 6px;\">\n";
-	echo "<li><a href="testbank_cs.php">Computer Science</a></li>";
-	}
-	echo "</ul>\n";
-	echo <<<HEREDOC
-
-
-HEREDOC;
+	<ul style="list-style: disc inside; margin-left: 6px;">
+	<li><a href="testbank_cs.php">Computer Science</a></li>
+	</ul>
 }
 Template::print_body_footer();
 Template::print_disclaimer();
