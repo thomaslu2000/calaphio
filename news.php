f75812c07ce55a0bab21b0d2644192466c8d94c0
aaytsai <tsai.audrey@berkeley.edu>
2015-08-24 20:26:35 -0700

    error

--- news.php
+++ news.php
@@ -7,6 +7,7 @@ require("include/EvalNag.class.php");
 require("include/GGManiacNag.class.php");
 Template::print_head(array("site.css", "calendar.css", "excel.css"));
 Template::print_body_header('Home', 'NEWS');
+ini_set('display_errors',1);  error_reporting(E_ALL);
 
 $evalnag = new EvalNag();
 echo $evalnag->display("2007-01-01");
