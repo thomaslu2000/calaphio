c2e0c6d1919a62345bf70fff423c74620c670279
aaytsai <tsai.audrey@berkeley.edu>
2015-09-30 22:43:48 -0700

    update_chairs

--- mh_chairs.php
+++ mh_chairs.php
@@ -8,12 +8,12 @@ if (!$g_user->is_logged_in()) {
 	trigger_error("You must be logged in to view the chairing positions.", E_USER_ERROR);
 } else {
 	echo <<<HEREDOC
-<h1 style="margin-bottom: 1em;">TT Semester Committees</h1>
+<h1 style="margin-bottom: 1em; text-align: center">PMP Semester Committees</h1>
 
 <font color="red">* Position</font>: Email the ExComm members or chair of any commitee to join!<br>
 <br>
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Presidential Committees: Karen Wu
+Presidential: Karen Wu
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
@@ -23,40 +23,43 @@ None! BUT, check out some of the other awesome committees below!
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Administrative Committees: Jason Lee
+Administrative: Audrey Tsai
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
-<b>Stylus:</b> Stylus Chair: Elizabeth Yuen, Trinh Huynh, Dana Lin<br>
-*Submit one or more article in the bi-weekly stylus! <br><br>
-<b>FunPack:</b> Elizabeth Yuen, Bertha Chui, Lisa Hoang<br>
+<b>Stylus:</b> Stylus Chair: Patrick Chang, Mitchell Lui, Antony Nguyen <br>
+*Like writing, photography, art, comics, interviews? Submit one or more creative pieces to our chapter bi-weekly newsletter! <br><br>
+<b>FunPack:</b> Lisa Hoang, Claudia Lim <br>
 *Help us create the end of the semester yearbook! No design experience needed!<br><br>
 </p>
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Membership Chairs: Rebecca Phuong
+Membership: Nicki Bartak
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
 	
-<b>Rush:</b> Bella Tsay, Joanna Choi, Nicki Bartak, Antony Nguyen<br>
+<b>Rush:</b> Joseph Gapuz, Ya-An Hsiung, Dian Jiang, Elizabeth Yuen, James Young<br>
 *Come help us recruit some AWESOME new pledges! <font color "red">Note: This committee is now closed</font><br><br>
-<b>Roll Call:</b> Tenzin Paldon, Sangmo Arya, Chris Wen<br>
+<b>Roll Call:</b> Claudia Lim, Antony Nguyen, Alex Quan <br>
 *Come rep Gamma Gamma at Sectionals by participating in Roll Call! No dance experience necessary!<br><br>
 </p>
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Service Committees: Debbbie Yan
+Service: Alex Quan
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
 	
-<b>Spring Youth Service Day:</b> Virgil Tang, Joanna Choi, Sangmo Arya<br>
-*Help up make crafts and design games for Spring Youth Service Day<br><br>	
+<b> Halloween Carnival Committee:</b> Sophia Du, Kelly Luu, Annie Yu <br>
+*Help up make crafts and design games for local elementary children in our annual Halloween Carnival!<br><br>
+<b> Service Project Committee:</b> Elise Hayashi, Dian Jiang, Ramya Rupanagudi, Hannah Schnell  <br>
+*Help plan informational workshops, fun exercises, and activities for kids for Healthy Heroes Day on Saturday, November 21st!<br><br>	
 </p>
 </div>
+
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
 Finance Chairs: Kelsey Chan
@@ -70,20 +73,20 @@ None! But you can help the Finance department by signing up for fundraisers!
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Fellowship Committees: Ann Chan
+Fellowship Committees: Allison Tong
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
 
-<b>Hot Spot</b>: Dana Lin, Tenzin Paldon, Winnie looc, Jeffrey Kuan, Allison Tong, Kirk Chiu<br>
-* Come help us set up the hottest-spot in town! <font color "red">Note: This committee is now full! Sorry! </font> <br><br>
-<b> Banquet</font>: Susan Guan, Ellie Hung, April Liu, Jane Tam<br>
+<b>Hot Spot</b>: Christine Fang, Selena Fung, Eugenia Tang, Estelle Yeung, Tiffany Young <br>
+* Come help us set up the hottest-spot in town! <br><br>
+<b> Banquet</font>: Elaine Do, Anqi Li, Eugenia Tang, Annie Yu<br>
 *Like to decorate? Plan events? BANQUENT COMMITTEE WANTS YOU! (Pledges welcome to join!)<br>
 </p>
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Pledgemaster Committees: James Wang
+Pledgemaster Committees: Joseph Gapuz
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
@@ -94,17 +97,15 @@ Pledgemaster Committees: James Wang
 </div>
 
 <h2 style="font-size: 1.3em; font-weight: bold; border-bottom: solid 1px black;">
-Historian Committees: Lakana Bun
+Historian Committees: Moncarol Wang
 </h2>
 <div style="max-width: 50em; margin-bottom: 3em; line-height: 1.5;">
 	<p style="margin: 1.5em 0px;">
 
-<b>Scrapbook</b>:Elizabeth Yuen, Sherri Zhang<br>
+<b>Scrapbook</b>: Allyssa Rodriguez <br>
 *Help us make the end-of semester Scrapbook! <br><br>
-<b>Photography</b>: Benjamin Le, Yoyo Tsai, Moncarol Wang<br>
-*Join the photo committee by taking pictures at events and uploading them to smugmug! Professional Camera not required!<br><br>
-<b>Chapter Wiki Chair:</b> Yoyo Tsai, Kathleen Wong, Pooja Shah<br>
-*Help the Wiki Chairs in creating a new Chapter Wiki! No programming experince required!
+
+
 </p>
 </div>
 
