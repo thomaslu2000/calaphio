06c99350effbdeb3887efe7c1273d82338d0f0c1
Audrey Tsai <tsai.audrey@berkeley.edu>
2016-01-31 22:30:20 +0100

    edited_Settings_location

--- include/includes.php
+++ include/includes.php
@@ -9,7 +9,7 @@ if (isset($_REQUEST['sid'])) {
 session_start();
 header("Cache-control: private"); // Hack to fix IE sessions
 ob_start();
-require('settings/Settings.php');
+require('/home/calaphio/configs/members.calaphio.com/Settings.php');
 //require('/home/calaphi/settings/Settings.php');
 if (SESSION_DEBUG) {
 	print_r($_SESSION);
