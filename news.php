cb8cfa263db49c59ab066611fd681bfb8c568012
tojerrry <aaytsai@gmail.com>
2016-04-11 11:10:23 +0200

    elections

--- news.php
+++ news.php
@@ -43,8 +43,13 @@ if (!$g_user->is_logged_in()) {
     candidates in preparation. </br> 
     <br> 
     <a href="https://docs.google.com/document/d/1Y4K2GaPYeCWTyvdV_ogeQESonxJy6Mqin5mwNllFqlE" target="_blank"> Pledgemaster Platform Kirk Chiu </a> </a><br>
+    <a href="https://docs.google.com/document/d/1Ri2Dw2M15cq9kDrK9WyrOF4tdFPROuap9zAMJXaee7w" target="_blank"> Pledgemaster Platform Ellie Hung</a> </a><br>
+    <a href="https://docs.google.com/document/d/1QBp0ehhCEIQZ8eo9VOi5kW0rz-60Jzojk0SX4ajZF24" target="_blank"> Admin VP Carrie Lin</a> </a><br>
     <a href="https://docs.google.com/document/d/1rLWbfIdBVxl8CcmioBx87py0rxXOTqjTHFITBBhmnGw" target="_blank"> Admin VP James Young </a> </a><br>
-    <a href="https://docs.google.com/document/d/14HLIsonQaN6aXNse7BL92Cc5AwpTpy0UIB130RjqmLU" target="_blank"> Historian Stanley Shaw </a> </a><br>
+    <a href="https://docs.google.com/document/d/14HLIsonQaN6aXNse7BL92Cc5AwpTpy0UIB130RjqmLU" target="_blank"> Historian VP Stanley Shiau </a> </a><br>
+    <a href="https://docs.google.com/document/d/1caehLk1PBUCqGoAfjFLCOA2p_lbbNXIotMjW_dfmxKA" target="_blank"> Membership VP Antony Nguyen </a> </a><br>
+    <a href="https://docs.google.com/document/d/1uAleZeVtbwpt7gRPs0kcvpYClA1wlNdG3xO0KMY-AIM" target="_blank"> Service VP Elaine Do </a> </a><br>
+    <a href="https://docs.google.com/document/d/1sl_cNXRlmQ8p4IPpnm1HBuXA9J6uZ36E_g5v8tVqNng" target="_blank"> Service VP Christine Fang </a> </a><br>
 
     <p>-<a href="profile.php?user_id=2859">Trinh Huynh (KHK)</a></p>
 <div> 
