b29c1da8a87df4f0902b55650eab900d54aeae79
Jerry Jongho Park <jerryjonghopark@gmail.com>
2017-02-01 00:54:18 -0800

    update maniacs

--- gg_maniacs.php
+++ gg_maniacs.php
@@ -10,6 +10,26 @@ Template::print_body_header('Home', 'NEWS');
 ?>
 <h1>Gamma Gamma Maniacs!</h1>
 <div class="newsItem">
+    <h2 class="center">MMC Semester GG Maniacs!</h2>
+    <div class="collage-container">
+	<div class="collage-pictures">
+		<div class="person-picture">
+			<p class="center">Charles Wang</p>
+		</div>
+	</div>
+	<div style="clear: left;"></div>
+    </div>
+</div>
+<div class="newsItem">
+    <h2 class="center">MMC Semester Pledge Maniacs!</h2>
+    <div class="collage-container">
+	<div class="collage-pictures">
+		
+	</div>
+	<div style="clear: left;"></div>
+    </div>
+</div>
+<div class="newsItem">
     <h2 class="center">FH Semester GG Maniacs!</h2>
     <div class="collage-container">
 	<div class="collage-pictures">
