9e08c30e90c6866bd842ab0d88645fe42f702119
James Young <jyoung96@berkeley.edu>
2017-02-08 21:32:37 -0800

    temporary poll access to try to fix stanley's ability to see who votes

--- gg_maniac_poll.php
+++ gg_maniac_poll.php
@@ -4,7 +4,7 @@ require("include/Template.class.php");
 require("include/Calendar.class.php");
 Template::print_head(array());
 Template::print_body_header('Home', 'ADMIN');
-if (!$g_user->is_logged_in() || !$g_user->permit("admin change passphrase") || $g_user->data['user_id'] != 3292) /* 3292 = Stan's ID */ {
+if (!$g_user->is_logged_in() || !$g_user->permit("admin change passphrase") || $g_user->data['user_id'] != (2978 || 3292)) /* 3292 = Stan's ID */ {
 	trigger_error("You must be logged in as a historian to access this feature", E_USER_ERROR);
 } else {
 
