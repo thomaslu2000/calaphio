e06c26421661efbb04bb1fb7a7a4b94372c87850
Jerry Jongho Park <jerryjonghopark@gmail.com>
2017-03-07 01:49:57 -0800

    fellowship buds

--- apo_fellowship_buddy.php
+++ apo_fellowship_buddy.php
@@ -12,7 +12,7 @@ if (!$g_user->is_logged_in() || !$g_user->permit("admin view requirements")) {
 	$id = $_REQUEST['id'];
 	$query = new Query(sprintf("
 		SELECT concat(users.lastname, ', ', users.firstname) AS name, concat(buddy.lastname, ', ', buddy.firstname) AS buddy, event.title AS title, event.event_id as event_id, event.type_service_chapter AS service_chapter, event.type_service_community AS service_community, event.type_service_country AS service_country, event.type_service_campus AS service_campus, event.type_fundraiser AS fundraiser, user_attend.hours AS hours, event.* FROM apo_calendar_attend AS user_attend
-		JOIN apo_calendar_event AS event ON (user_attend.event_id = event.event_id AND (event.type_service_campus = TRUE OR event.type_service_chapter = TRUE OR event.type_service_community = TRUE OR event.type_service_country = TRUE OR event.type_fundraiser = TRUE OR event.type_fellowship = TRUE OR event.type_interchapter = TRUE))
+		JOIN apo_calendar_event AS event ON (user_attend.event_id = event.event_id AND event.type_fellowship = TRUE)
 		JOIN apo_fellowship_buddy AS user_buddy ON (user_attend.user_id = user_buddy.user_id AND event.date >= user_buddy.begin AND event.date <= user_buddy.end) 
 		JOIN apo_calendar_attend AS buddy_attend ON (user_buddy.buddy_id = buddy_attend.user_id AND user_attend.event_id = buddy_attend.event_id) 
 		JOIN apo_users AS users ON (user_attend.user_id = users.user_id) 
