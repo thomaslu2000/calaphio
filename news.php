c83a501d3931bb89ecbc2c17bb09b253195cf1d1
James Young <jyoung96@berkeley.edu>
2017-08-27 22:13:55 -0700

    I think I made a php error oops

--- news.php
+++ news.php
@@ -31,96 +31,10 @@ $g_user->print_personal_messages();
 
 
 
-<!-- <?php if ($g_user->is_logged_in()): ?>
-<div class="newsItem">
-        <h2>Fall 2016 Executive Committee Chairs</h2>
-        <p class="date">August 5, 2016 at 12:54pm</p>
-
-    <p style="margin: 1.5em 0px;">
-        * Denotes chairing positions with committees
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>PRESIDENT:</u></b><br>
-        <b>* Fall Fellowship Chairs:</b> Gene Ho, Bianca Hsueh, Nick Weis, Stanley Shaw<br>
-        <b>Parliamentarian:</b> Joseph Lee<br>
-        <b>Sergeant-at-Arms:</b> Calvin Lui, Richard Wong, Naomichi Yamamoto<br>
-        <b>IC/Public Relations Chairs:</b> Gordon Mah, Veronica Hall<br>
-        <b>* Professional Development Chair:</b> Henry Chen<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>ADMIN VP:</u></b><br>
-        <b>Admin VP Assistant:</b> Jerry Park<br>
-        <b>Webmasters:</b> Jeffrey Li, Thaniel Directo, Yuki Mizuno, Edson Romero, Audrey Tsai<br>
-        <b>* Stylus Chairs:</b> Bianca Hsueh, Claire Li, Miranda Zhou<br>
-        <b>* Funpack Chairs:</b> Gordon Mah, Nina Nguyen, Lisa Hoang<br>
-        <b>Academic Chair:</b> Hermes Ip<br>
-    </p>
-
-     <p style="margin: 1.5em 0px;">
-        <b><u>MEMBERSHIP VP:</u></b><br>
-        <b>* Rush Chairs:</b> Gene Ho, Alice Hsieh, Jessica Tzeng, Scottie Wan<br>
-        <b>Dynasty Directors:</b> Joanna Choi, Sierra Lou, Tenzin Paldon, Sherri Zhang, Patrick Chang, Claudia Lim<br>
-        <b>Membership VP Assistants:</b> Alice Hsieh, Jessica Tzeng, Yanan Wang, Veronica Hall<br>
-        <b>Gear Chairs:</b> Bianca Hsueh, Nina Nguyen<br>
-        <b>* Roll Call Chairs:</b> Jessica Lee, Miranda Zhou, Jeremy Lam, Serena Wu<br>
-        <b>Sunshine Chairs:</b> Leona Chen, Spencer Liu<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>SERVICE VP:</u></b><br>
-        <b>Service VP Assistants:</b> Kerry Feng, Nina Nguyen, Miranda Zhou<br>
-        <b>* Hallcarn Chairs:</b> Leona Chen, Kerry Feng, Claire Li, Gordon Mah, Isaac Zheng<br>
-        <b>Active Day of Service Chair:</b> William Liao, Sierra Lou<br>
-        <b>IC/GG Sewing Chairs:</b> Kerry Feng, Joanna Choi<br>
-        <b>* Campus-Wide Service Project Chairs:</b> Jessica Lee, Nick Weis, Audrey Tsai, Scottie Wan<br>
-        <b>YTA Beartrax/Mosswood Chairs:</b> Molly Caldwell, Stanley Shaw<br>
-        <b>Project Open Hand Chair:</b> Jeffrey Li<br>
-        <b>Berkeley Food and Housing Chair:</b> Gene Ho<br>
-        <b>Service Buddies Chairs:</b> Isaac Zheng, Claudia Lim<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>FINANCE VP:</u></b><br>
-        <b>Finance VP Assistants:</b> Hermes Ip, Jessica Tzeng<br>
-        <b>Fundraiser Chairs:</b> Joshua Jacobs, Jessica Lee, Jerry Park, Hyeonji Shim<br>
-        <b>Reimbursement Chairs:</b> Leona Chen, Alexander Feng<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>FELLOWSHIP VP:</u></b><br>
-        <b>Fellowship VP Assistants:</b> Gene Ho, Spencer Liu<br>
-        <b>* Hotspot Chairs:</b> Dinasha Dahanayake, Yanan Wang, Nick Weis, Yitian Zhang, Yuki Mizuno<br>
-        <b>GG Sports Chairs:</b> Jayant Raman, Naomichi Yamamoto<br>
-        <b>GG Events Chairs:</b> Selena Fung, Ya-An Hsiung<br>
-        <b>IC Events Chair:</b> Karen Chou<br>
-        <b>* Banquet Chairs:</b> Joshua Jacobs, Jessica Tzeng, Claudia Lim, Joanna Choi<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>PLEDGEMASTER:</u></b><br>
-        <b>Leadership Trainer:</b> Jerianne Lukban<br>
-        <b>Fellowship Trainers:</b> Hyeonji Shim, Naomichi Yamamoto<br>
-        <b>Service Trainer:</b> Virginia Yan<br>
-        <b>Finance Trainers:</b> Joseph Lee, Jerry Park<br>
-        <b>Administrative Trainer:</b> Karen Chou<br>
-        <b>Historian Trainer:</b> Jeremy Lam<br>
-    </p>
-
-    <p style="margin: 1.5em 0px;">
-        <b><u>HISTORIAN:</u></b><br>
-        <b>Historian Assistant:</b> Stanley Shaw<br>
-        <b>Alumni Relations Chairs:</b> William Liao, Gordon Mah, Jeremy Lam, Audrey Tsai<br>
-        <b>* Scrapbook Chairs:</b> Molly Caldwell, Kira Wong<br>
-        <b>* Photography Chairs:</b> Adrian Peneyra, Jerry Park<br>
-        <b>GG Maniac Chairs:</b> Kerry Feng, Kira Wong<br>
-        <b>Chapter Wiki Chair:</b> Chris Janssen<br>
-    </p>
-    
-    <p>- <a href="profile.php?user_id=2978">James Young (TT)</a></p>
-</div>
-<?php endif ?> -->
+if (!$g_user->is_logged_in()) {
+    echo '<img style="float: right" src="images/lfs_banner.png" alt="LFS" />';
+}
+?>
 
 <div class="newsItem">
         <h2>Welcome Gamma Gamma!</h2>
@@ -128,7 +42,7 @@ $g_user->print_personal_messages();
 
     <br>Since school is almost starting, it's about time to get back into the APO mentality. Just remember that we're all students first and need to prioritize school, and to focus on quality service over quantity. Good luck this semester!</br> 
     
-    <p>- <a href="profile.php?user_id=2978">James E Young (TT)</a></p>
+    <p>- <a href="profile.php?user_id=3571">Bianca Hsueh (RBD)</a></p>
 </div>
 
 <?php if ($g_user->is_logged_in() && !$g_user->is_pledge()): ?>
