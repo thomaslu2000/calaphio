41d68620a01af95e225d018de286ccd55e4f79bb
James Young <jyoung96@berkeley.edu>
2017-09-30 17:50:03 -0700

    changed superstars start time

--- admin_superstar.php
+++ admin_superstar.php
@@ -33,7 +33,7 @@ if (!$g_user->is_logged_in()) {
 	trigger_error("You must be logged in to view this page.");
 } else {
 $import_http_request = array(
-	"start" => date("2016-12-06"),
+	"start" => date("2017-05-03"),
 	"end" => date("Y-m-d", strtotime("now"))
 	);
 foreach ($import_http_request as $var => $default) {
