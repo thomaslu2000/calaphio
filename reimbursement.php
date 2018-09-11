be92b5e9d5bdc19f22ad665e5a2a3bb412416eee
Shengmin Xiao <shengminx@Shengmins-MacBook-Pro.local>
2018-09-10 21:38:02 -0700

    Reimbursement Fa18

--- reimbursement.php
+++ reimbursement.php
@@ -5,15 +5,14 @@ require("include/Template.class.php");
 Template::print_head(array());
 Template::print_body_header('Calendar', 'REIMBURSE');
 ?>
-<h2><b>REIMBURSEMENTS/b></h2><br/>
+<h2><b>REIMBURSEMENTS</h2><br/>
 
-<b>You are required to fill out the following form:</b><br/>
-https://goo.gl/forms/7ad3kGYINWXaCNNx2
+<p>You are required to fill out the following <a href="https://goo.gl/forms/wl4B7QyoexsN5R4c2" target="_blank">FORM</a><br/>
 
 
-<p><b>Electronic Receipts</b> - Submit necessary documents on google form<br/><br/>
+<p>Electronic Receipts - Submit necessary documents on google form<br/><br/>
 
-<p><b>Physical Copy of Receipts</b> - write down these information for reimbursements  <br/>
+<p>Physical Copy of Receipts - write down these information for reimbursements  <br/>
 and give it to either <a href="profile.php?user_id=4803">Samantha Wang</a>, <a href="profile.php?user_id=4795">Kevin Chuang</a>, or <a href="profile.php?user_id=4796">Evelyn Chan</a>: <br/><br/>
 Name <br/>
 SID <br/>
@@ -26,6 +25,8 @@ Don't forget that you can get half your public transportation fee reimbursed too
 such as BART tickets if you get the receipt by paying with credit card. <br />
 However, if you cross any toll bridge, ask for a receipt and you'll get reimbursed full if you are driving. <br />
 *Note: these only pertain to service events <br />
+
+
 
 <?php
 Template::print_body_footer();
