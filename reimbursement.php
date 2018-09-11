e619cd1e311f2ab920ff74b2b0e5d9ab8cc9ea08
Shengmin Xiao <shengminx@Shengmins-MacBook-Pro.local>
2018-09-10 21:57:34 -0700

    reimbursement

--- reimbursement.php
+++ reimbursement.php
@@ -7,6 +7,13 @@ Template::print_body_header('Calendar', 'REIMBURSE');
 ?>
 
 
+<?php if ($g_user->is_logged_in()): ?>
+
+
+	<h2><b>REIMBURSEMENTS</h2><br/>
+
+	<p>You are required to fill out the following <a href="https://goo.gl/forms/wl4B7QyoexsN5R4c2" target="_blank">FORM</a><br/>
+
 
 
 
