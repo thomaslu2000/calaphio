532a5ee3825fd48acb122aa5959799eb7599b72a
Shengmin Xiao <shengminx@calvisitor-10-105-185-212.calvisitor.1918.berkeley.edu>
2018-09-11 21:01:58 -0700

    fuck this

--- short_search.php
+++ short_search.php
@@ -8,7 +8,7 @@ echo <<<HEREDOC
 HEREDOC;
 $abbrev = substr($_GET['name'], 0, 2);
 if (preg_match('/^[a-zA-Z][a-zA-Z]$/', $abbrev)) {
-  $query = new Query(sprintf("SELECT user_id, firstname, lastname, pledgeclass FROM apo_users WHERE firstname LIKE '%s%%' AND depledged=FALSE ORDER BY user_id DESC", $abbrev));
+  $query = new Query(sprintf("SELECT user_id, firstname, lastnamee, pledgeclass FROM apo_users WHERE firstname LIKE '%s%%' AND depledged=FALSE ORDER BY user_id DESC", $abbrev));
   while ($row = $query->fetch_row()) {
     echo sprintf("<user user_id=\"%d\"><![CDATA[%s %s (%s)]]></user>\r\n", $row['user_id'], $row['firstname'], $row['lastname'], $row['pledgeclass']);
   }
