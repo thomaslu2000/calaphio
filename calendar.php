a31d34d0fed35b8d59c9983f71e9e830a05150d2
Thomas <t.lu@email.com>
2019-03-08 13:33:18 -0800

    link to google calendar

--- calendar.php
+++ calendar.php
@@ -5,6 +5,8 @@ require("include/Template.class.php");
 Template::print_head(array("site.css", "calendar.css"));
 Template::print_body_header('Calendar', 'CALENDAR');
 
+echo "<a href='https://calendar.google.com/calendar?cid=cTFodGJpcDE0YjVrMmo4Zmg5Z29hMGVxdDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'> Link to Google Calendar </a>";
+
 $calendar = new Calendar();
 $calendar->print_month();
 
