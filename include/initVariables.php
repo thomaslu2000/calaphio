88b31ab9db8ca9b0819c748e8b137fdcd6108899
Thomas <t.lu@email.com>
2019-03-27 20:16:25 -0700

    removed a test

--- include/initVariables.php
+++ include/initVariables.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1nGL7EFeRhQ29fJM0GRoQS_ZgHgY5GZs2rRDUNLiIRaI/edit?usp=sharing";
 $GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/1UHi-jXJE81ivCynymgVezzy69_cPS-x5Jxgfaf0HBPE/edit?fbclid=IwAR0cgax6eLDxahp3ITOPdCjGpuFn0UgvpBp304mgfqQ17S6sUWXXk37RyW4#gid=1589878783';
-$GLOBALS['hide all fams'] = true;
+$GLOBALS['hide all fams'] = false;
 $GLOBALS['smugmug rush'] = "https://calaphio.smugmug.com/upload/wbHrmR/rush";
 $GLOBALS['smugmug service'] = "https://calaphio.smugmug.com/upload/M2JDwn/service";
 $GLOBALS['smugmug fellowship'] = "https://calaphio.smugmug.com/upload/HV9RSN/fellowships";
