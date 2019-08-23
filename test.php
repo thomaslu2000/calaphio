b9e9e02261258567014cb7450efeed3d60638bc2
Thomas <t.lu@email.com>
2019-08-22 18:36:57 -0700

    new test file

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php
 require("include/includes.php");
-require("include/testCalendar.class.php");
 require("include/Template.class.php");
-Template::print_head(array("site.css", "calendar.css"));
-Template::print_body_header('Calendar', 'CALENDAR');
-$calendar = new Calendar();
-$calendar->print_month();
+Template::print_head(array());
+Template::print_body_header('Home', '');
+
+
+$permitted = $g_user->permit("admin view requirements");
+echo "$permitted";
+
 Template::print_body_footer();
 Template::print_disclaimer();
 ?>
