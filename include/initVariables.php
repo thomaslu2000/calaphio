7b2e6abfc18a31e84639358360105dbd2233b50b
Thomas <t.lu@email.com>
2019-09-02 17:31:06 -0700

    update budget/chairing sheet/hide families

--- include/initVariables.php
+++ include/initVariables.php
@@ -1,8 +1,8 @@
 <?php
 
-$GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1nGL7EFeRhQ29fJM0GRoQS_ZgHgY5GZs2rRDUNLiIRaI/edit?usp=sharing";
-$GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/1UHi-jXJE81ivCynymgVezzy69_cPS-x5Jxgfaf0HBPE/edit?fbclid=IwAR0cgax6eLDxahp3ITOPdCjGpuFn0UgvpBp304mgfqQ17S6sUWXXk37RyW4#gid=1589878783';
-$GLOBALS['hide all fams'] = false;
+$GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1qLt0kQL7v9Y3G4ucab7AON8q3kQgNMh69E1qOlL_LcU/edit?usp=sharing";
+$GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/1GqlGposJv6hACqMulxbTTvnC7qbppX5dLTN5tGPT6Mk/edit?usp=sharing';
+$GLOBALS['hide all fams'] = true;
 $GLOBALS['smugmug rush'] = "https://calaphio.smugmug.com/upload/wbHrmR/rush";
 $GLOBALS['smugmug service'] = "https://calaphio.smugmug.com/upload/TcKh9q/allenwongsummer";
 $GLOBALS['smugmug fellowship'] = "https://calaphio.smugmug.com/upload/HV9RSN/fellowships";
