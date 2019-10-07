4671c8fb0f06960b16e28daa612e32bc916b847a
Thomas <t.lu@email.com>
2019-10-06 18:46:28 -0700

    unhide fams since after sib social

--- include/initVariables.php
+++ include/initVariables.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1qLt0kQL7v9Y3G4ucab7AON8q3kQgNMh69E1qOlL_LcU/edit?usp=sharing";
 $GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/1GqlGposJv6hACqMulxbTTvnC7qbppX5dLTN5tGPT6Mk/edit?usp=sharing';
-$GLOBALS['hide all fams'] = true;
+$GLOBALS['hide all fams'] = false;
 $GLOBALS['smugmug rush'] = "https://calaphio.smugmug.com/upload/wbHrmR/rush";
 $GLOBALS['smugmug service'] = "https://calaphio.smugmug.com/upload/TcKh9q/allenwongsummer";
 $GLOBALS['smugmug fellowship'] = "https://calaphio.smugmug.com/upload/HV9RSN/fellowships";
