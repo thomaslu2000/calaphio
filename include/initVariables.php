8ed4c7b4225f07caddd0dfdf0065fe46de2ed2f8
Aakarsh Aithal <aakarshaithal@Aakarshs-MacBook-Pro.local>
2020-02-12 17:00:00 -0800

    rehid fams

--- include/initVariables.php
+++ include/initVariables.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1jEC7c6BF3gDVNQhAsq0T7Zh2LxX-_dze75tHIHTY9r0/edit#gid=1283827056";
 $GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/19HEbOs9iq1DD6G9CILyX3P1kp1Sealw_sG_YIZettrg/edit';
-$GLOBALS['hide all fams'] = false;
+$GLOBALS['hide all fams'] = true;
 $GLOBALS['smugmug rush'] = "https://calaphio.smugmug.com/upload/wbHrmR/rush";
 $GLOBALS['smugmug service'] = "https://calaphio.smugmug.com/upload/TcKh9q/allenwongsummer";
 $GLOBALS['smugmug fellowship'] = "https://calaphio.smugmug.com/upload/HV9RSN/fellowships";
