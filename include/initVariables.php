cebf85ac135528db44d5dea081ca875f6dbd1e32
Aakarsh Aithal <aakarshaithal@Aakarshs-MacBook-Pro.local>
2020-02-12 16:31:33 -0800

    hid families

--- include/initVariables.php
+++ include/initVariables.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['budget'] = "https://docs.google.com/spreadsheets/d/1jEC7c6BF3gDVNQhAsq0T7Zh2LxX-_dze75tHIHTY9r0/edit#gid=1283827056";
 $GLOBALS['excomm committees'] = 'https://docs.google.com/spreadsheets/d/1GqlGposJv6hACqMulxbTTvnC7qbppX5dLTN5tGPT6Mk/edit?usp=sharing';
-$GLOBALS['hide all fams'] = false;
+$GLOBALS['hide all fams'] = true;
 $GLOBALS['smugmug rush'] = "https://calaphio.smugmug.com/upload/wbHrmR/rush";
 $GLOBALS['smugmug service'] = "https://calaphio.smugmug.com/upload/TcKh9q/allenwongsummer";
 $GLOBALS['smugmug fellowship'] = "https://calaphio.smugmug.com/upload/HV9RSN/fellowships";
